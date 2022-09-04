f58c668758c8a41cec74a9c3e83e2981ccaa46ff
szuwgh <895340293@qq.com>
2022-09-04 23:07:28 +0800

    coding

--- src/util/image.rs
+++ src/util/image.rs
@@ -1,5 +1,11 @@
-pub(crate) struct Image {}
+pub(crate) struct ModelConfig {
+    model_path: String,
+}
 
-impl Image {
-    pub(crate) fn load() {}
+pub(crate) struct ImageEmbed {}
+
+impl ImageEmbed {
+    pub(crate) fn load_model(m: ModelConfig) {}
+
+    pub(crate) fn embed(image_opt: Image) {}
 }
